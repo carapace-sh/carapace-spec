92d966a491537c70737c4edeaca7943b0f858af6
rsteube <rsteube@users.noreply.github.com>
2023-09-08 23:18:27 +0200

    flag modifier test

--- flag_test.go
+++ flag_test.go
@@ -1,9 +1,12 @@
 package spec
 
 import (
+	_ "embed"
 	"reflect"
 	"testing"
 
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace/pkg/sandbox"
 	"github.com/spf13/pflag"
 )
 
@@ -117,5 +120,35 @@ func TestParseFlag(t *testing.T) {
 		shorthand:       "short",
 		longhand:        "long",
 		nameAsShorthand: false,
+	})
+}
+
+//go:embed example/flag.yaml
+var flagSpec string
+
+func TestFlag(t *testing.T) {
+	sandboxSpec(t, flagSpec)(func(s *sandbox.Sandbox) {
+		s.Run("--hidden").
+			Expect(carapace.ActionValues().
+				NoSpace('.'))
+
+		s.Run("--hidden-arg", "").
+			Expect(carapace.ActionValues(
+				"h1",
+				"h2",
+			).Usage("hidden with argument"))
+
+		s.Run("--hidden-opt=").
+			Expect(carapace.ActionValues(
+				"ho1",
+				"ho2",
+			).Prefix("--hidden-opt=").
+				Usage("hidden with optional argument"))
+
+		s.Run("--repeatable", "--repeat").
+			Expect(carapace.ActionValuesDescribed(
+				"--repeatable", "repeatable",
+			).NoSpace('.').
+				Tag("flags"))
 	})
 }
