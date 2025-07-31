eca79ea6ac93a31c6bf25883e8ce7a1a203b2086
rsteube <rsteube@users.noreply.github.com>
2025-07-31 21:31:50 +0200

    tmp

--- pkg/command/run.go
+++ pkg/command/run.go
@@ -1,11 +1,44 @@
 package command
 
-import "github.com/spf13/cobra"
+import (
+	"strings"
 
-type run struct {
-	r interface {
-		parse() func(cmd *cobra.Command, args []string) error
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
+)
+
+type runnable interface {
+	parse() func(cmd *cobra.Command, args []string) error
+}
+
+type run struct{ runnable }
+
+func (r run) MarshalYAML() ([]byte, error) {
+	return yaml.Marshal(r.runnable)
+}
+
+func (r *run) UnmarshalYAML(b []byte) error {
+	var m any
+	if err := yaml.Unmarshal(b, &m); err != nil {
+		return err
 	}
+
+	switch m := m.(type) {
+	case []string:
+		r.runnable = alias(m)
+	case string:
+		switch {
+		case strings.HasPrefix(m, "$"):
+			r.runnable = macro(m)
+		case strings.HasPrefix(m, "#!"):
+			r.runnable = shebang(m)
+		case strings.HasPrefix(m, "["):
+			// TODO legacy alias
+		}
+	default:
+		// invalid
+	}
+	return nil // TODO invalid?
 }
 
 type alias []string
@@ -27,11 +60,11 @@ func (s shebang) parse() func(cmd *cobra.Command, args []string) error {
 }
 
 func Alias(command string, args ...string) run {
-	return run{r: alias(append([]string{command}, args...))}
+	return run{alias(append([]string{command}, args...))}
 }
 func Macro(s string) run {
-	return run{r: macro(s)}
+	return run{macro(s)}
 }
 func Shebang(s string) run {
-	return run{r: shebang(s)}
+	return run{shebang(s)}
 }
