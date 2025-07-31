1ad725927215778e43cfac9682bb0911f65d415e
rsteube <rsteube@users.noreply.github.com>
2025-07-31 19:43:15 +0200

    run: support shebang

---
+++ pkg/command/run.go
@@ -0,0 +1,275 @@
+package command
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"runtime"
+	"strings"
+
+	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace/pkg/execlog"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"gopkg.in/yaml.v3"
+)
+
+type Run string
+
+func Alias(s ...string) (Run, error) {
+	if len(s) == 0 {
+		return "", errors.New("invalid alias")
+	}
+
+	var alias = struct { // pseudo-struct to enforce `flow` style
+		A []string `yaml:",flow"`
+	}{s}
+
+	m, err := yaml.Marshal(alias)
+	if err != nil {
+		return "", err
+	}
+	return Run(m[3:]), nil // cut `a: ` prefix
+}
+
+func (r Run) Type() string { // TODO return custom type
+	switch s := string(r); {
+	case strings.HasPrefix(s, "$"):
+		return "macro"
+	case strings.HasPrefix(s, "#!"):
+		return "script"
+	case strings.HasPrefix(s, "["):
+		return "alias"
+	default:
+		return ""
+	}
+}
+
+func (r *Run) UnmarshalYAML(value *yaml.Node) error {
+	var script string
+	if err := value.Decode(&script); err == nil {
+		*r = Run(script)
+		return nil
+	}
+
+	var alias []string
+	if err := value.Decode(&alias); err != nil {
+		return err
+	}
+
+	var err error
+	*r, err = Alias(alias...)
+	return err
+}
+
+func (r Run) Parse() func(cmd *cobra.Command, args []string) error {
+	switch r.Type() {
+	case "macro":
+		return r.parseMacro()
+	case "script":
+		return r.parseScript()
+	case "alias": // legacy
+		return r.parseAlias()
+	default:
+		return nil // TODO handle the error somehow (log or give feedback)
+	}
+}
+
+func (r Run) parseAlias() func(cmd *cobra.Command, args []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		context := carapace.NewContext(args...)
+		cmd.Flags().VisitAll(func(f *pflag.Flag) { // VisitAll as Visit() skips changed persistent flags of parent commands
+			if f.Changed {
+				if slice, ok := f.Value.(pflag.SliceValue); ok {
+					context.Setenv(fmt.Sprintf("C_FLAG_%v", strings.ToUpper(f.Name)), strings.Join(slice.GetSlice(), ","))
+				} else {
+					context.Setenv(fmt.Sprintf("C_FLAG_%v", strings.ToUpper(f.Name)), f.Value.String())
+				}
+			}
+		})
+
+		mCmd := ""
+		mArgs := make([]string, 0)
+		if err := yaml.Unmarshal([]byte(r), &mArgs); err != nil {
+			return err
+		}
+		if len(mArgs) < 1 {
+			return fmt.Errorf("malformed alias: %#v", r)
+		}
+
+		mCmd = mArgs[0]
+		mArgs = mArgs[1:]
+
+		var err error
+		for index, arg := range mArgs {
+			if mArgs[index], err = context.Envsubst(arg); err != nil {
+				return err
+			}
+		}
+
+		execCmd := execlog.Command(mCmd, append(mArgs, args...)...)
+		execCmd.Stdin = cmd.InOrStdin()
+		execCmd.Stdout = cmd.OutOrStdout()
+		execCmd.Stderr = cmd.ErrOrStderr()
+		execCmd.Env = context.Env
+		if err := execCmd.Run(); err != nil {
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				os.Exit(exitErr.ProcessState.ExitCode())
+			}
+			return err
+		}
+		return nil
+	}
+}
+
+func (r Run) parseMacro() func(cmd *cobra.Command, args []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		context := carapace.NewContext(args...)
+		cmd.Flags().VisitAll(func(f *pflag.Flag) { // VisitAll as Visit() skips changed persistent flags of parent commands
+			if f.Changed {
+				if slice, ok := f.Value.(pflag.SliceValue); ok {
+					context.Setenv(fmt.Sprintf("C_FLAG_%v", strings.ToUpper(f.Name)), strings.Join(slice.GetSlice(), ","))
+				} else {
+					context.Setenv(fmt.Sprintf("C_FLAG_%v", strings.ToUpper(f.Name)), f.Value.String())
+				}
+			}
+		})
+
+		mCmd := ""
+		mArgs := make([]string, 0)
+
+		matches := regexp.MustCompile(`^\$(?P<macro>[^(]*)(\((?P<arg>.*)\))?$`).FindStringSubmatch(string(r))
+		if matches == nil {
+			return fmt.Errorf("malformed macro: %#v", r)
+		}
+
+		script, err := context.Envsubst(matches[3])
+		if err != nil {
+			return err
+		}
+
+		mCmd = "sh"
+		switch matches[1] {
+		case "":
+			if runtime.GOOS == "windows" {
+				mCmd = "pwsh"
+			}
+
+		case "bash", "elvish", "fish", "ion", "nu", "osh", "pwsh", "sh", "xonsh", "zsh":
+			mCmd = matches[1]
+
+		default:
+			return fmt.Errorf("unknown macro: %#v", matches[1])
+		}
+
+		switch mCmd {
+		case "nu", "pwsh":
+			// nu and pwsh handle arguments after `-c` differently (https://github.com/PowerShell/PowerShell/issues/13832)
+
+			suffix := ".nu"
+			if mCmd == "pwsh" {
+				suffix = ".ps1"
+			}
+
+			path, err := os.CreateTemp(os.TempDir(), "carapace-spec_run*"+suffix)
+			if err != nil {
+				return err
+			}
+			defer os.Remove(path.Name())
+
+			if err = os.WriteFile(path.Name(), []byte(script), 0700); err != nil {
+				return err
+			}
+			if err := path.Close(); err != nil {
+				return err
+			}
+			mArgs = append(mArgs, path.Name())
+
+		default:
+			mArgs = append(mArgs, "-c", script, "--")
+		}
+
+		execCmd := exec.Command(mCmd, append(mArgs, args...)...)
+		execCmd.Stdin = cmd.InOrStdin()
+		execCmd.Stdout = cmd.OutOrStdout()
+		execCmd.Stderr = cmd.ErrOrStderr()
+		execCmd.Env = context.Env
+		if err := execCmd.Run(); err != nil {
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				os.Exit(exitErr.ProcessState.ExitCode())
+			}
+			return err
+		}
+		return nil
+	}
+}
+
+type shebang struct {
+	Command string
+	Args    []string // optional single argument
+}
+
+func (r Run) parseShebang() (*shebang, error) {
+	firstLine, _, ok := strings.Cut(string(r), "\n")
+	if !ok {
+		return nil, errors.New("missing shebang header")
+	}
+
+	re := regexp.MustCompile(`^#!(?P<command>[^ ]+)( (?P<arg>.*))?$`)
+	matches := re.FindStringSubmatch(strings.TrimSpace(firstLine))
+	if matches == nil {
+		return nil, errors.New("invalid shebang header")
+	}
+
+	shebang := &shebang{
+		Command: matches[1],
+		Args:    []string{},
+	}
+	if matches[3] != "" {
+		shebang.Args = []string{matches[3]} // optional arg
+	}
+
+	return shebang, nil
+}
+
+func (r Run) parseScript() func(cmd *cobra.Command, args []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		// TODO currently duplicated in each run type
+		context := carapace.NewContext(args...)
+		cmd.Flags().VisitAll(func(f *pflag.Flag) { // VisitAll as Visit() skips changed persistent flags of parent commands
+			if f.Changed {
+				if slice, ok := f.Value.(pflag.SliceValue); ok {
+					context.Setenv(fmt.Sprintf("C_FLAG_%v", strings.ToUpper(f.Name)), strings.Join(slice.GetSlice(), ","))
+				} else {
+					context.Setenv(fmt.Sprintf("C_FLAG_%v", strings.ToUpper(f.Name)), f.Value.String())
+				}
+			}
+		})
+
+		shebang, err := r.parseShebang()
+		if err != nil {
+			return err
+		}
+
+		file, err := os.CreateTemp(os.TempDir(), "carapace-spec_run")
+		if err != nil {
+			return err
+		}
+		defer os.Remove(file.Name())
+
+		os.WriteFile(file.Name(), []byte(r), os.ModePerm) // TODO make only readable by current user
+
+		scriptArgs := append(shebang.Args, file.Name())
+		scriptArgs = append(scriptArgs, args...)
+
+		scriptCmd := execlog.Command(shebang.Command, scriptArgs...)
+		scriptCmd.Stdout = cmd.OutOrStdout()
+		scriptCmd.Stderr = cmd.ErrOrStderr()
+		scriptCmd.Stdin = cmd.InOrStdin()
+		scriptCmd.Env = context.Env
+		// TODO support dir potentially modified by `$chdir()` modifier
+		return scriptCmd.Run()
+	}
+}
