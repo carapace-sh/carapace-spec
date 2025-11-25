2c20c2cff3b3b69377db808b1fdf86c4dc0d5197
rsteube <rsteube@users.noreply.github.com>
2025-11-25 13:56:36 +0100

    tmp

--- flag.go
+++ flag.go
@@ -3,125 +3,115 @@ package spec
 import (
 	"fmt"
 
+	"github.com/carapace-sh/carapace-spec/pkg/command"
 	"github.com/spf13/pflag"
 )
 
-type flag struct {
-	longhand        string
-	shorthand       string
-	usage           string
-	slice           bool
-	optarg          bool
-	value           bool
-	nameAsShorthand bool
-	hidden          bool
-	required        bool
-	nargs           int
-}
+type flag command.Flag
 
 func (f flag) addTo(fset *pflag.FlagSet) error {
 	fs := flagSet{fset}
-	if len(f.shorthand) > 1 && !fs.IsFork() {
-		return fmt.Errorf("long shorthand only supported with carapace-sh/carapace-pflag: %v", f.shorthand)
+	if len(f.Shorthand) > 1 && !fs.IsFork() {
+		return fmt.Errorf("long shorthand only supported with carapace-sh/carapace-pflag: %v", f.Shorthand)
 	}
-	if f.longhand == "" && !fs.IsFork() {
-		return fmt.Errorf("shorthand-only only supported with carapace-sh/carapace-pflag: %v", f.shorthand)
+	if f.Longhand == "" && !fs.IsFork() {
+		return fmt.Errorf("shorthand-only only supported with carapace-sh/carapace-pflag: %v", f.Shorthand)
 	}
-	if f.nargs != 0 && !fs.IsFork() {
-		return fmt.Errorf("nargs only supported with carapace-sh/carapace-pflag: %v", f.shorthand)
+	if f.Nargs != 0 && !fs.IsFork() {
+		return fmt.Errorf("nargs only supported with carapace-sh/carapace-pflag: %v", f.Shorthand)
 	}
 
-	if f.longhand != "" && f.shorthand != "" {
-		if f.value {
-			if f.slice {
-				if f.nameAsShorthand {
-					fs.StringSliceN(f.longhand, f.shorthand, []string{}, f.usage)
+	if f.Longhand != "" && f.Shorthand != "" {
+		if f.Value {
+			if f.Slice {
+				if f.NameAsShorthand {
+					fs.StringSliceN(f.Longhand, f.Shorthand, []string{}, f.Description)
 				} else {
-					fs.StringSliceP(f.longhand, f.shorthand, []string{}, f.usage)
+					fs.StringSliceP(f.Longhand, f.Shorthand, []string{}, f.Description)
 				}
 			} else {
-				if f.nameAsShorthand {
-					fs.StringN(f.longhand, f.shorthand, "", f.usage)
+				if f.NameAsShorthand {
+					fs.StringN(f.Longhand, f.Shorthand, "", f.Description)
 				} else {
-					fs.StringP(f.longhand, f.shorthand, "", f.usage)
+					fs.StringP(f.Longhand, f.Shorthand, "", f.Description)
 				}
 			}
 		} else {
-			if f.slice {
-				if f.nameAsShorthand {
-					fs.CountN(f.longhand, f.shorthand, f.usage)
+			if f.Slice {
+				if f.NameAsShorthand {
+					fs.CountN(f.Longhand, f.Shorthand, f.Description)
 				} else {
-					fs.CountP(f.longhand, f.shorthand, f.usage)
+					fs.CountP(f.Longhand, f.Shorthand, f.Description)
 				}
 			} else {
-				if f.nameAsShorthand {
-					fs.BoolN(f.longhand, f.shorthand, false, f.usage)
+				if f.NameAsShorthand {
+					fs.BoolN(f.Longhand, f.Shorthand, false, f.Description)
 				} else {
-					fs.BoolP(f.longhand, f.shorthand, false, f.usage)
+					fs.BoolP(f.Longhand, f.Shorthand, false, f.Description)
 				}
 			}
 		}
-	} else if f.longhand != "" {
-		if f.value {
-			if f.slice {
-				if f.nameAsShorthand {
-					fs.StringSliceS(f.longhand, f.longhand, []string{}, f.usage)
+	} else if f.Longhand != "" {
+		if f.Value {
+			if f.Slice {
+				if f.NameAsShorthand {
+					fs.StringSliceS(f.Longhand, f.Longhand, []string{}, f.Description)
 				} else {
-					fs.StringSlice(f.longhand, []string{}, f.usage)
+					fs.StringSlice(f.Longhand, []string{}, f.Description)
 				}
 			} else {
-				if f.nameAsShorthand {
-					fs.StringS(f.longhand, f.longhand, "", f.usage)
+				if f.NameAsShorthand {
+					fs.StringS(f.Longhand, f.Longhand, "", f.Description)
 				} else {
-					fs.String(f.longhand, "", f.usage)
+					fs.String(f.Longhand, "", f.Description)
 				}
 			}
 		} else {
-			if f.slice {
-				if f.nameAsShorthand {
-					fs.CountS(f.longhand, f.longhand, f.usage)
+			if f.Slice {
+				if f.NameAsShorthand {
+					fs.CountS(f.Longhand, f.Longhand, f.Description)
 				} else {
-					fs.Count(f.longhand, f.usage)
+					fs.Count(f.Longhand, f.Description)
 				}
 			} else {
-				if !f.nameAsShorthand {
-					fs.Bool(f.longhand, false, f.usage)
+				if !f.NameAsShorthand {
+					fs.Bool(f.Longhand, false, f.Description)
 				} else {
-					fs.BoolS(f.longhand, f.longhand, false, f.usage)
+					fs.BoolS(f.Longhand, f.Longhand, false, f.Description)
 				}
 			}
 		}
-	} else if f.shorthand != "" {
-		if f.value {
-			if f.slice {
-				fs.StringSliceS(f.shorthand, f.shorthand, []string{}, f.usage)
+	} else if f.Shorthand != "" {
+		if f.Value {
+			if f.Slice {
+				fs.StringSliceS(f.Shorthand, f.Shorthand, []string{}, f.Description)
 			} else {
-				fs.StringS(f.shorthand, f.shorthand, "", f.usage)
+				fs.StringS(f.Shorthand, f.Shorthand, "", f.Description)
 			}
 		} else {
-			if f.slice {
-				fs.CountS(f.shorthand, f.shorthand, f.usage)
+			if f.Slice {
+				fs.CountS(f.Shorthand, f.Shorthand, f.Description)
 			} else {
-				fs.BoolS(f.shorthand, f.shorthand, false, f.usage)
+				fs.BoolS(f.Shorthand, f.Shorthand, false, f.Description)
 			}
 		}
 	}
 
-	if f.optarg {
-		if f.longhand != "" {
-			fs.Lookup(f.longhand).NoOptDefVal = " "
+	if f.Optarg {
+		if f.Longhand != "" {
+			fs.Lookup(f.Longhand).NoOptDefVal = " "
 		} else {
-			fs.Lookup(f.shorthand).NoOptDefVal = " "
+			fs.Lookup(f.Shorthand).NoOptDefVal = " "
 		}
 	}
 
-	if f.hidden {
-		fs.Lookup(f.longhand).Hidden = f.hidden
+	if f.Hidden {
+		fs.Lookup(f.Longhand).Hidden = f.Hidden
 	}
 
-	if f.nargs != 0 {
+	if f.Nargs != 0 {
 		// TODO nargs only exists in fork
-		fs.Lookup(f.longhand).Nargs = f.nargs
+		fs.Lookup(f.Longhand).Nargs = f.Nargs
 	}
 
 	return nil
